bf3ea6d8f32f7428c9c984ba0ec6fdb4514611d9
Yaroslav Antonenko <yantonenko@adyax.com>
2019-08-14 16:30:41 +0300

    Chapter 3 second commit

---
+++ ch3/a/tasks_proj/tests/conftest.py
@@ -0,0 +1,51 @@
+import pytest
+import tasks
+from tasks import Task
+
+
+@pytest.fixture()
+def tasks_db(tmpdir):
+    """Connect to db before tests, disconnect after."""
+    # Setup : start db
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
+
+    yield  # this is where test happens
+
+    # Teardown : stop db
+    tasks.stop_tasks_db()
+
+
+# Reminder of Task constructor interface
+# Task(summary=None, owner=None, done=False, id=None)
+# summary is required
+# owner and done are optional
+# id is set by database
+
+@pytest.fixture()
+def tasks_just_a_few():
+    """all summaries and owners are required"""
+    return (
+        Task('Write some code', 'Brian', True),
+        Task("Code review Brian's code", 'Katie', False),
+        Task('Fix what Brian did', 'Michelle', False)
+    )
+
+
+@pytest.fixture()
+def tasks_mult_per_owner():
+    """several owners with several tasks each"""
+    return (
+        Task('Make a cookie', 'Raphael'),
+        Task('Use an emoji', 'Raphael'),
+        Task('Move to Berlin', 'Raphael'),
+
+        Task('Create', 'Michelle'),
+        Task('Inspire', 'Michelle'),
+        Task('Encourage', 'Michelle'),
+
+        Task('Do a handstand', 'Daniel'),
+        Task('Write some books', 'Daniel'),
+        Task('Eat ice cream', 'Daniel')
+    )
+
+
--- ch3/test_fixtures.py
+++ ch3/test_fixtures.py
@@ -6,8 +6,18 @@ def some_data():
     """return answer"""
     return 42
 
+@pytest.fixture()
+def a_tuple():
+    """Return something more interesting."""
+    return (1, 'foo', None, {'bar': 23})
+
 
 def test_some_data(some_data):
     """use fixture value in a test"""
     assert some_data == 42
+
+
+def test_a_tuple(a_tuple):
+    """Demo for a_tuple fixture"""
+    assert a_tuple[3]['bar'] == 32
 
