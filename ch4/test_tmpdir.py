e50e618ad53eb6d68be3b858b20917e679fe321f
Yaroslav Antonenko <yantonenko@adyax.com>
2019-08-22 17:23:20 +0300

    Chapter 4 second commit (93)

--- ch4/test_tmpdir.py
+++ ch4/test_tmpdir.py
@@ -9,3 +9,23 @@ def test_tmpdir(tmpdir):
     another_file.write('something different')
     assert a_file.read() == 'contents may settle during shipping'
     assert another_file.read() == 'something different'
+
+
+def test_tmpdir_factory(tmpdir_factory):
+    # you should start with making a directory
+    # a_dir acts like the object returned from the tmpdir fixture
+    a_dir = tmpdir_factory.mktemp('mydir')
+
+    # base_temp is a parent dir of a_dir
+    base_temp = tmpdir_factory.getbasetemp()
+    print('\nbase: ', base_temp)
+
+    a_file = a_dir.join('something.txt')
+    a_sub_dir = a_dir.mkdir('anything')
+    another_file = a_sub_dir.join('something_else.txt')
+
+    a_file.write('contents may settle during shipping')
+    another_file.write('something different')
+
+    assert a_file.read() == 'contents may settle during shipping'
+    assert another_file.read() == 'something different'
