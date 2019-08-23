67db83d82496226b22860f18296c4bef685e196b
Yaroslav Antonenko <yantonenko@adyax.com>
2019-08-23 17:06:58 +0300

    Chapter 4 second commit (97)

--- ch4/pytestconfig/test_config.py
+++ ch4/pytestconfig/test_config.py
@@ -2,4 +2,20 @@ import pytest
 
 
 def test_option(pytestconfig):
-    
+    print('"foo" set to:', pytestconfig.getoption('foo'))
+    print('"myopt" set to', pytestconfig.getoption('myopt'))
+
+
+@pytest.fixture()
+def foo(pytestconfig):
+    return pytestconfig.option.foo
+
+
+@pytest.fixture()
+def myopt(pytestconfig):
+    return pytestconfig.option.myopt
+
+
+def test_fixtures_for_options(foo, myopt):
+    print('"myopt" is:', myopt)
+    print(('"foo" is:', foo))
