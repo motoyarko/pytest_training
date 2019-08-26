699e410829644aef6469dc5664a1ce522bd599c3
Yaroslav Antonenko <yantonenko@adyax.com>
2019-08-26 17:20:49 +0300

    Chapter 4 third commit (103)

--- ch4/pytestconfig/conftest.py
+++ ch4/pytestconfig/conftest.py
@@ -1,3 +1,3 @@
 def pytest_addoption(parser):
-    parser.addoption("--myopt", action="store_true", help="some boolean option")
+    parser.addoption("--myopt", action="store_true", default="none", help="some boolean option")
     parser.addoption("--foo", action="store", default="bar", help="foo: bar on baz")
